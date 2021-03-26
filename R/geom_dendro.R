73cce463afbdc05d4281922d62daa26d9d7df523
Nicolas Huber <50340626+NicolasH2@users.noreply.github.com>
2021-03-26 16:31:47 +0100

    Update geom_dendro.R

--- R/geom_dendro.R
+++ R/geom_dendro.R
@@ -102,17 +102,9 @@ geom_dendro <- function(clust, xlim=NULL, ylim=NULL, pointing="updown", dendrocu
   
   #if desired by the user, define axis labels for x or y axis, depending on if the dendrogram points down/up or sideways
   if(axis.labels){
-    if(pointing %in% "updown"){
-      ggplotlabel <- list(
-        ggplot2::scale_x_continuous(breaks=plotlabels$x, labels=plotlabels$label),
-        ggplot2::scale_y_continuous(expand=c(0,0))
-      )
-    }else{
-      ggplotlabel <- list(
-        ggplot2::scale_y_continuous(breaks=plotlabels$y, labels=plotlabels$label),
-        ggplot2::scale_x_continuous(expand=c(0,0))
-      )
-    }
+    ggplotlabel <- ifelse(pointing %in% "updown",
+                          ggplot2::scale_x_continuous(breaks=plotlabels$x, labels=plotlabels$label),
+                          ggplot2::scale_y_continuous(breaks=plotlabels$y, labels=plotlabels$label))
     
     output <- c(output, ggplotlabel) #combine the dendrogram with the axis label information
   }
